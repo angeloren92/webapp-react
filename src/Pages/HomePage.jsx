ca6c3d56a7536faf61bdfc6fcd3841f798f242ea
angeloren92 <angeloren92@gmail.com>
2026-04-21 12:22:48 +0200

    add main homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,42 @@
 export default function Homepage() {
 
-  return (
-    <>
-        <h1>homepage</h1>
-    </>
-  )
+    return (
+        <section>
+            <div className="container">
+                <h1 className="text-center my-4">Benvenuti su CineReview</h1>
+                <div className="row">
+                    <figure className="col d-flex flex-column align-items-start justify-content-center">
+                        <div className="card">
+                            <img src="https://placehold.co/400x600/png" alt="" className="img-fluid" />
+                            <div className="card-body">
+                                <h2 className="h3">Benvenuti su CineReview</h2>
+                                <p className="lead">Il tuo punto di riferimento per recensioni e consigli sui film.</p>
+                                <a href="#" className="btn btn-primary">Scopri di più</a>
+                            </div>
+                        </div>
+                    </figure>
+                    <figure className="col d-flex flex-column align-items-start justify-content-center">
+                        <div className="card">
+                            <img src="https://placehold.co/400x600/png" alt="" className="img-fluid" />
+                            <div className="card-body">
+                                <h2 className="h3">Benvenuti su CineReview</h2>
+                                <p className="lead">Il tuo punto di riferimento per recensioni e consigli sui film.</p>
+                                <a href="#" className="btn btn-primary">Scopri di più</a>
+                            </div>
+                        </div>
+                    </figure>
+                    <figure className="col d-flex flex-column align-items-start justify-content-center">
+                        <div className="card">
+                            <img src="https://placehold.co/400x600/png" alt="" className="img-fluid" />
+                            <div className="card-body">
+                                <h2 className="h3">Benvenuti su CineReview</h2>
+                                <p className="lead">Il tuo punto di riferimento per recensioni e consigli sui film.</p>
+                                <a href="#" className="btn btn-primary">Scopri di più</a>
+                            </div>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+        </section>
+    )
 }
